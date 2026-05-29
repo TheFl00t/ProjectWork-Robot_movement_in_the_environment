1be7155c2bff84a40e551898f518dc9df2cbd44b
thefl00t <thefl00ttt@gmail.com>
2026-05-29 18:04:09 +0300

    feat: introduce RenderStyle and unify fragment shader using uniforms

--- src/model/Entity.h
+++ src/model/Entity.h
@@ -3,21 +3,36 @@
 #include "../config.h"
 #include "../view/Mesh.h"
 
+// Режими відтворення геометрії
+enum class DrawMode {
+    Outline,       // Тільки контур
+    Fill,          // Тільки заливка
+    FillAndOutline // Заливка + контур
+};
+
+// Структура стану візуалу (Матеріал)
+struct RenderStyle {
+    DrawMode mode          = DrawMode::Outline;
+    glm::vec4 fillColor    = glm::vec4(1.0f); // Колір заливки
+    glm::vec4 outlineColor = glm::vec4(1.0f); // Колір контуру
+    float lineWidth        = 1.0f;            // Товщина ліній контуру
+};
+
 class Entity {
 protected:
     Mesh* mesh;
 
 public:
     glm::vec2 entityPos;
+    RenderStyle style; // Вся графічна конфігурація зберігається тут
     
     Entity(glm::vec2 pos = glm::vec2(0.0f, 0.0f)) 
         : entityPos(pos), mesh(nullptr) {};
     
     virtual ~Entity() {};
-
     virtual void update(float dt) = 0;
 
-    // Робота з мешем
+    // Робота з мешем об'єкта
     void setMesh(Mesh* m) { mesh = m; }
     Mesh* getMesh() const { return mesh; }
 };
